2a7c8df255a5ca810c27d11a7482b3da84d2a92c
Hector G. Parra <hector@hectorparra.com>
2013-09-26 01:20:33 -0700

    Refactored out BackgroundImagerCLI and added tests
    Primary functional test for CLI is now uses mocha

--- bin/background-imager-cli.js
+++ bin/background-imager-cli.js
@@ -57,9 +57,7 @@
 //
 
 var program = require("commander"),
-    bgi = require("../lib/background-imager.js"),
-    ifh = require("../lib/image-filename-helper.js"),
-    fs = require('fs');
+    cli = require("../lib/background-imager-cli");
 
 // options
 program
@@ -74,91 +72,13 @@ program
 if (process.argv.length < 3) {
     program.help();
 }
-
-// typical media query expression for mobile widths
-// TODO: make option
-var MEDIA_EXPRESSION_MOBILE_MAX_WIDTH = "(max-width: 480px)";
-
-// options specified
-var filepath = process.argv[2],
-    tabSpacing = program.tabSpacing,
-    cssOptions = {
-        urlPath: program.urlPath,
-        classPrefix: program.classPrefix,
-    };
-
-// escape \t and \s properly
-tabSpacing = tabSpacing.replace(/\\t/gi, '\t').replace(/\\s/gi, ' ');
-
-//
-// MAIN
-//
-
-(function() {
-
-    // read top-directory only (no nested directory support)
-    fs.readdir(filepath, function (err, files) {
-
+else {
+    cli.run(process.argv[2], program, function (err, data) {
         if (err) {
             console.error(err);
-            console.error("There was an error reading the directory.");
-            process.exit(1);
+            process.exit(1); 
         }
 
-        // filter image files from directory
-        var images = files.filter(ifh.isImage);
-
-        if (images.length === 0) {
-            console.error("There are no images in specified directory.");
-            process.exit(1);
-        }
-
-        // array of abstract (incomplete) MediaRules, one for each scenario:
-        // * 1x
-        // * 2x
-        // * mobile 1x
-        // * mobile 2x
-        //
-        // each MediaRule object (will) contain(s):
-        // * MediaQueries
-        // * RuleSets
-        // * image filenames (not part of CSS)
-        var abstractMediaRules = [{
-            // 1x
-            queries: null,
-            images: images.filter(ifh.isNotSmallImage).filter(ifh.is1xImage)
-        },{
-            // 2x
-            queries: bgi.generate2xMediaQueries(),
-            images: images.filter(ifh.isNotSmallImage).filter(ifh.is2xImage)
-        },{
-            // mobile 1x
-            queries: [
-                bgi.generateMediaQuery([
-                    MEDIA_EXPRESSION_MOBILE_MAX_WIDTH
-                ])
-            ],
-            images: images.filter(ifh.isSmallImage).filter(ifh.is1xImage)
-        },{
-            // mobile 2x
-            queries: bgi.generate2xMediaQueries([
-                MEDIA_EXPRESSION_MOBILE_MAX_WIDTH
-            ]),
-            images: images.filter(ifh.isSmallImage).filter(ifh.is2xImage)
-        }];
-
-        // create media rules
-        bgi.createMediaRules(abstractMediaRules, filepath, cssOptions, function(err, mediaRules) {
-
-            if (err) {
-                console.error(err);
-                console.error("Some MediaRule was not completed successfully");
-                process.exit(1);
-            }
-
-            // print CSS
-            console.log(bgi.generateCSS(mediaRules, tabSpacing));
-        });
-
+        console.log(data);
     });
-})();
+}
