2f92ad0ac9ebaeff9074b47af7835a86bee775af
Hector G. Parra <hector@hectorparra.com>
2013-09-27 22:39:26 -0700

    Added parsing functions for media DSL.
    Also made some (now legacy) regex case-insensitive and less strict.

--- test/image-filename-helper.js
+++ test/image-filename-helper.js
@@ -3,20 +3,7 @@ var should = require('should'),
 
 describe("ImageFilenameHelper", function () {
 
-	var LEGAL_FILENAMES = [
-		"noodle.png",
-		"noodle.gif",
-		"noodle.jpg",
-		"noodle.jpeg",
-		"noodle.JPEG"
-	];
-
-	var ILLEGAL_FILENAMES = [
-		"noodle.ping",
-		"noodle.jpg.exe",
-		"noodle.psd"
-	];
-
+	// are these actually helpful? Makes tests harder to read
 	var HAS_NULL = "noodle.jpg",
 		HAS_1X = "noodle@1x.jpg",
 		HAS_1X_AND_640W = "noodle@1x^640w.jpg",
@@ -30,66 +17,298 @@ describe("ImageFilenameHelper", function () {
 		HAS_2X_AND_640W_OR_1X_REVERSED = "noodle@1x,640w^2x.jpg";
 
 	//
-	// isImage()
+	// Tests for @media parser methods
 	//
 
-	describe("#isImage", function () {
-		// true
-		it("should return true when file has a compatible image extension", function () {
-			for (var i = LEGAL_FILENAMES.length - 1; i >= 0; i--) {
-				helper.isImage(LEGAL_FILENAMES[i]).should.equal(true, LEGAL_FILENAMES[i]);
-			};
+	describe("(Parsers)", function() {
+
+		//
+		// parseMediaExpression
+		//
+
+		describe("#parseMediaExpression", function () {
+			// 640w
+			it("should return correct @media expression object when expression is \"640w\"", function () {
+				helper.parseMediaExpression("640w").should.include({
+					feature: "min-width",
+					value: 640
+				});
+			})
+			// 1x
+			it("should return correct @media expression object when expression is \"1x\"", function () {
+				helper.parseMediaExpression("1x").should.include({
+					feature: "min-device-pixel-ratio",
+					value: 1
+				});
+			})
+			// 1.5x
+			it("should return correct @media expression object when expression is \"1.5x\"", function () {
+				helper.parseMediaExpression("1.5x").should.include({
+					feature: "min-device-pixel-ratio",
+					value: 1.5
+				});
+			})
+			// 768h
+			it("should return correct @media expression object when expression is \"768h\"", function () {
+				helper.parseMediaExpression("768h").should.include({
+					feature: "min-height",
+					value: 768
+				});
+			})
+			// null
+			it("should throw error when expression is `null`", function () {
+				(function () {
+					helper.parseMediaExpression(null);
+				}).should.throwError(/expression/);
+			})
+			// ""
+			it("should throw error when expression is empty string", function () {
+				(function () {
+					helper.parseMediaExpression("");
+				}).should.throwError(/expression/);
+			})
+			// 640z
+			it("should throw error when expression is \"640z\"", function () {
+				(function () {
+					helper.parseMediaExpression("640z");
+				}).should.throwError(/expression/);
+			})
+			// x
+			it("should throw error when expression is \"x\"", function () {
+				(function () {
+					helper.parseMediaExpression("x");
+				}).should.throwError(/expression/);
+			})
+			// 640w^2x
+			it("should throw error when expression is \"640w^2x\"", function () {
+				(function () {
+					helper.parseMediaExpression("640w^2x");
+				}).should.throwError(/expression/);
+			})
 		})
-		// false
-		it("should return false when file does not have a (compatible) image extension", function () {
-			for (var i = ILLEGAL_FILENAMES.length - 1; i >= 0; i--) {
-				helper.isImage(ILLEGAL_FILENAMES[i]).should.equal(false, ILLEGAL_FILENAMES[i]);
-			};
+
+		//
+		// parseMediaQueries
+		//
+
+		describe("#parseMediaQueries", function () {
+			// ""
+			it("should return correct media queries array when query string is empty", function () {
+				helper.parseMediaQueries("").should.be.instanceOf(Array).and.be.empty;
+			});
+			// 1x
+			it("should return correct media queries array when query string is \"1x\"", function () {
+				helper.parseMediaQueries("1x").should.be.instanceOf(Array)
+					.and.includeEql(
+						[{
+							feature: "min-device-pixel-ratio",
+							value: 1
+						}]
+					);
+			});
+			// 1x^640w
+			it("should return correct media queries array when query string is \"1x^640w\"", function () {
+				helper.parseMediaQueries("1x^640w").should.be.instanceOf(Array)
+					.and.includeEql(
+						[{
+							feature: "min-device-pixel-ratio",
+							value: 1
+						},{
+							feature: "min-width",
+							value: 640
+						}]
+					);
+			});
+			// 640w^2x,1x
+			it("should return correct media queries array when query string is \"640w^2x,1x\"", function () {
+				helper.parseMediaQueries("640w^2x,1x").should.be.instanceOf(Array)
+					.and.includeEql(
+						[{
+							feature: "min-width",
+							value: 640
+						},{
+							feature: "min-device-pixel-ratio",
+							value: 2
+						}]
+					)
+					.and.includeEql(
+						[{
+							feature: "min-device-pixel-ratio",
+							value: 1
+						}]
+					);
+			})
+			// null
+			it("should throw error when query string is `null`", function () {
+				(function () {
+					helper.parseMediaQueries(null);
+				}).should.throwError(/rule/);
+			})
+		})
+
+		//
+		// parseFilename
+		//
+
+		describe("#parseFilename", function () {
+			// noodle.jpg
+			it("should return correct image file object when filename is \"noodle.jpg\"", function () {
+				var obj = helper.parseFilename("noodle.jpg");
+				obj.should.be.instanceOf(Object);
+				obj.should.have.property("name", "noodle");
+				obj.should.have.property("extension", "jpg");
+				obj.should.have.property("queries")
+					.and.be.instanceOf(Array).and.be.empty;
+			})
+			// noodle@1x^640w.jpg
+			it("should return correct image file object when filename is \"noodle@1x^640w.jpg\"", function () {
+				var obj = helper.parseFilename("noodle@1x^640w.jpg");
+				obj.should.be.instanceOf(Object);
+				obj.should.have.property("name", "noodle");
+				obj.should.have.property("extension", "jpg");
+				obj.should.have.property("queries")
+					.and.be.instanceOf(Array)
+					.and.includeEql(
+						[{
+							feature: "min-device-pixel-ratio",
+							value: 1
+						},{
+							feature: "min-width",
+							value: 640
+						}]
+					);
+			})
+			// noodle@640w^2x,1x.jpg
+			it("should return correct image file object when filename is \"noodle@640w^2x,1x.jpg\"", function () {
+				var obj = helper.parseFilename("noodle@640w^2x,1x.jpg");
+				obj.should.be.instanceOf(Object);
+				obj.should.have.property("name", "noodle");
+				obj.should.have.property("extension", "jpg");
+				obj.should.have.property("queries")
+					.and.be.instanceOf(Array)
+					.and.includeEql(
+						[{
+							feature: "min-width",
+							value: 640
+						},{
+							feature: "min-device-pixel-ratio",
+							value: 2
+						}]
+					)
+					.and.includeEql(
+						[{
+							feature: "min-device-pixel-ratio",
+							value: 1
+						}]
+					);
+			})
 		})
 	})
 
 	//
-	// is1xImage()
+	// Tests for helpers
 	//
 
-	describe("#is1xImage", function () {
-		it("should return true when file has `1x` descriptor", function () {
-			helper.is1xImage(HAS_1X).should.equal(true, HAS_1X);
-			helper.is1xImage(HAS_1X_AND_640W).should.equal(true, HAS_1X_AND_640W);
-			helper.is1xImage(HAS_1X_OR_2X_AND_640W).should.equal(true, HAS_1X_OR_2X_AND_640W);
-			helper.is1xImage(HAS_2X_AND_640W_OR_1X).should.equal(true, HAS_2X_AND_640W_OR_1X);
-			helper.is1xImage(HAS_1X_AND_640W_REVERSED).should.equal(true, HAS_1X_AND_640W_REVERSED);
-			helper.is1xImage(HAS_1X_OR_2X_AND_640W_REVERSED).should.equal(true, HAS_1X_OR_2X_AND_640W_REVERSED);
-			helper.is1xImage(HAS_2X_AND_640W_OR_1X_REVERSED).should.equal(true, HAS_2X_AND_640W_OR_1X_REVERSED);
+	describe("(Helpers)", function() {
+
+		//
+		// isImageFilenameWithMedia()
+		//
+
+		describe("#getMediaSubstring", function () {
+			// noodle.jpg
+			it("should return `null` when filename is \"noodle.jpg\"", function () {
+				should.equal(helper.getMediaSubstring("noodle.jpg"), null);
+			})
+			// noodle@1x.jpg
+			it("should return correct substring when filename is \"noodle@1x.jpg\"", function () {
+				helper.getMediaSubstring("noodle@1x.jpg").should.equal("1x");
+			})
+			// noodle@1x^640w.jpg
+			it("should return correct substring when filename is \"noodle@1x^640w.jpg\"", function () {
+				helper.getMediaSubstring("noodle@1x^640w.jpg").should.equal("1x^640w");
+			})
+			// noodle@640w^2x,1x.jpg
+			it("should return correct substring when filename is \"noodle@1x^640w.jpg\"", function () {
+				helper.getMediaSubstring("noodle@640w^2x,1x.jpg").should.equal("640w^2x,1x");
+			})
 		})
-		it("should return false when file does not have `1x` descriptor", function () {
-			helper.is1xImage(HAS_NULL).should.equal(false, HAS_NULL);
-			helper.is1xImage(HAS_2X).should.equal(false, HAS_2X);
-			helper.is1xImage(HAS_2X_AND_640W).should.equal(false, HAS_2X_AND_640W);
-			helper.is1xImage(HAS_2X_AND_640W_REVERSED).should.equal(false, HAS_2X_AND_640W_REVERSED);
+
+		//
+		// isImage()
+		//
+
+		describe("#isImage", function () {
+			// true
+			it("should return true when file has a compatible image extension", function () {
+				helper.isImage("noodle.png").should.equal(true);
+				helper.isImage("noodle.gif").should.equal(true);
+				helper.isImage("noodle.jpeg").should.equal(true);
+				helper.isImage("noodle.jpg").should.equal(true);
+			})
+			// false
+			it("should return false when file does not have a (compatible) image extension", function () {
+				helper.isImage("noodle.ping").should.equal(false);
+				helper.isImage("noodle.psd").should.equal(false);
+				helper.isImage("noodle.pdf").should.equal(false);
+				helper.isImage("noodle.bmp").should.equal(false);
+			})
 		})
 	})
 
 	//
-	// is2xImage()
+	// DEPRECATED
 	//
 
-	describe("#is2xImage", function () {
-		it("should return true when file has `2x` descriptor", function () {
-			helper.is2xImage(HAS_2X).should.equal(true, HAS_2X);
-			helper.is2xImage(HAS_2X_AND_640W).should.equal(true, HAS_2X_AND_640W);
-			helper.is2xImage(HAS_1X_OR_2X_AND_640W).should.equal(true, HAS_1X_OR_2X_AND_640W);
-			helper.is2xImage(HAS_2X_AND_640W_OR_1X).should.equal(true, HAS_2X_AND_640W_OR_1X);
-			helper.is2xImage(HAS_1X_OR_2X_AND_640W_REVERSED).should.equal(true, HAS_1X_OR_2X_AND_640W_REVERSED);
-			helper.is2xImage(HAS_2X_AND_640W_OR_1X_REVERSED).should.equal(true, HAS_2X_AND_640W_OR_1X_REVERSED);
-			helper.is2xImage(HAS_2X_AND_640W_REVERSED).should.equal(true, HAS_2X_AND_640W_REVERSED);
-		})
-		it("should return false when file does not have `2x` descriptor", function () {
-			helper.is2xImage(HAS_NULL).should.equal(false, HAS_NULL);
-			helper.is2xImage(HAS_1X).should.equal(false, HAS_1X);
-			helper.is2xImage(HAS_1X_AND_640W).should.equal(false, HAS_1X_AND_640W);
-			helper.is2xImage(HAS_1X_AND_640W_REVERSED).should.equal(false, HAS_1X_AND_640W_REVERSED);
+	describe("(DEPRECATED)", function () {
 
+		//
+		// is1xImage()
+		//
+
+		describe("#is1xImage", function () {
+			// true
+			it("should return true when file has \"1x\" descriptor", function () {
+				helper.is1xImage(HAS_1X).should.equal(true, HAS_1X);
+				helper.is1xImage(HAS_1X_AND_640W).should.equal(true, HAS_1X_AND_640W);
+				helper.is1xImage(HAS_1X_OR_2X_AND_640W).should.equal(true, HAS_1X_OR_2X_AND_640W);
+				helper.is1xImage(HAS_2X_AND_640W_OR_1X).should.equal(true, HAS_2X_AND_640W_OR_1X);
+				helper.is1xImage(HAS_1X_AND_640W_REVERSED).should.equal(true, HAS_1X_AND_640W_REVERSED);
+				helper.is1xImage(HAS_1X_OR_2X_AND_640W_REVERSED).should.equal(true, HAS_1X_OR_2X_AND_640W_REVERSED);
+				helper.is1xImage(HAS_2X_AND_640W_OR_1X_REVERSED).should.equal(true, HAS_2X_AND_640W_OR_1X_REVERSED);
+			})
+			// false
+			it("should return false when file does not have \"1x\" descriptor", function () {
+				helper.is1xImage(HAS_NULL).should.equal(false, HAS_NULL);
+				helper.is1xImage(HAS_2X).should.equal(false, HAS_2X);
+				helper.is1xImage(HAS_2X_AND_640W).should.equal(false, HAS_2X_AND_640W);
+				helper.is1xImage(HAS_2X_AND_640W_REVERSED).should.equal(false, HAS_2X_AND_640W_REVERSED);
+			})
+		})
+
+		//
+		// is2xImage()
+		//
+
+		describe("#is2xImage", function () {
+			// true
+			it("should return true when file has \"2x\" descriptor", function () {
+				helper.is2xImage(HAS_2X).should.equal(true, HAS_2X);
+				helper.is2xImage(HAS_2X_AND_640W).should.equal(true, HAS_2X_AND_640W);
+				helper.is2xImage(HAS_1X_OR_2X_AND_640W).should.equal(true, HAS_1X_OR_2X_AND_640W);
+				helper.is2xImage(HAS_2X_AND_640W_OR_1X).should.equal(true, HAS_2X_AND_640W_OR_1X);
+				helper.is2xImage(HAS_1X_OR_2X_AND_640W_REVERSED).should.equal(true, HAS_1X_OR_2X_AND_640W_REVERSED);
+				helper.is2xImage(HAS_2X_AND_640W_OR_1X_REVERSED).should.equal(true, HAS_2X_AND_640W_OR_1X_REVERSED);
+				helper.is2xImage(HAS_2X_AND_640W_REVERSED).should.equal(true, HAS_2X_AND_640W_REVERSED);
+			})
+			// false
+			it("should return false when file does not have \"2x\" descriptor", function () {
+				helper.is2xImage(HAS_NULL).should.equal(false, HAS_NULL);
+				helper.is2xImage(HAS_1X).should.equal(false, HAS_1X);
+				helper.is2xImage(HAS_1X_AND_640W).should.equal(false, HAS_1X_AND_640W);
+				helper.is2xImage(HAS_1X_AND_640W_REVERSED).should.equal(false, HAS_1X_AND_640W_REVERSED);
+
+			})
 		})
 	})
 })
